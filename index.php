d4cbb4853f0baeee870a0578fc4eb8fe7f86efd1
宗伝 胡 <ksd523288@icloud.com>
2017-12-26 16:03:06 +0900

    test commit.

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+
+<form action="http://task-taskview.193b.starter-ca-central-1.openshiftapps.com/tasks/sync.json" method="post">
+    <textarea cols="80" rows="20" name="json"></textarea></br></br>
+    <input type="submit"/>
+</form>
+
+</html>
